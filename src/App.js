f3aeaa6287331681fa904e9afdfd31be93764274
Erika <erikamacedo2001@hotmail.com>
2024-04-13 20:25:09 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1>Hello, lembretes</h1>
+    </div>
+  );
+}
+
+export default App;
